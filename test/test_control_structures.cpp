02af61a395f316d8d790788a71ddccd7d479153f
Gareth George <garethgeorge97@gmail.com>
2018-04-17 17:43:23 -0700

    added test for basic while loops

---
+++ test/test_control_structures.cpp
@@ -0,0 +1,17 @@
+#include "../lib/catch.hpp"
+
+#include "include/test_helpers.hpp"
+
+TEST_CASE("while loops should loop", "[control -> while loop]") {
+    SECTION( "adding two values" ) {
+        auto code = build_string(R"(
+x = 0
+while x < 100:
+    x += 1
+check_int(x)
+        )");
+        InterpreterState state(code);
+        state.ns_builtins["check_int"] = make_builtin_check_value((int64_t)100);
+        state.eval();
+    }
+}
