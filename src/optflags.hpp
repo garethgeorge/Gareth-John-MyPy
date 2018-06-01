5e558b9c83b5e4eeefe82479ce121c8ea126696e
Jahwffrey <johnthomason@umail.ucsb.edu>
2018-06-01 12:47:15 -0700

    Fixed DUP_TOP_TWO and added ROT_THREE

--- src/optflags.hpp
+++ src/optflags.hpp
@@ -9,18 +9,18 @@
 #define DIRECT_THREADED
 #define RECYCLING_ON
 
-// #define DEBUG_ON
+//#define DEBUG_ON
 // #define DEBUG_STACK
 
 //#define JOHN_DEBUG_ON
 
-#define PROFILING_ON
+//#define PROFILING_ON
 
 // Output a timestamp at every opcode
-#define PER_OPCODE_PROFILING
+//#define PER_OPCODE_PROFILING
 
-#define GARBAGE_COLLECTION_PROFILING
+//#define GARBAGE_COLLECTION_PROFILING
 
-#define TYPE_INFORMATION_PROFILING
+//#define TYPE_INFORMATION_PROFILING
 
 #endif
